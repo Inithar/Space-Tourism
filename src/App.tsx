2419182a1afa7f608354093aa841a94e21604c20
Szymon Świtała <szymon.switalla@gmail.com>
2022-08-17 10:37:13 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
+import { Crew } from './pages/crew/Crew';
+import { Destination } from './pages/destination/Destination';
+import { Home } from './pages/home/Home';
+import { Technology } from './pages/technology/Technology';
 import GlobalStyle from './styles/global.styles';
 import { theme } from './styles/theme';
 
@@ -7,7 +13,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      Test
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="destination" element={<Destination />} />
+        <Route path="crew" element={<Crew />} />
+        <Route path="technology" element={<Technology />} />
+      </Routes>
     </ThemeProvider>
   );
 }
