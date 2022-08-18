b4052e50aaf9b1d2fed553eb20fff5d4d061ef18
Szymon Świtała <szymon.switalla@gmail.com>
2022-08-18 15:52:58 +0200

    added mobile navigation

--- src/components/navigation/Navigation.styles.tsx
+++ src/components/navigation/Navigation.styles.tsx
@@ -4,6 +4,11 @@ import { NavLink } from 'react-router-dom';
 import { device } from '../../styles/responsive';
 import logoIcon from '../../assets/shared/logo.svg';
 import burgerIcon from '../../assets/shared/icon-hamburger.svg';
+import closeIcon from '../../assets/shared/icon-close.svg';
+
+interface MobileNavigationProps {
+  isOpen: boolean;
+}
 
 export const Wrapper = styled.div`
   position: relative;
@@ -107,4 +112,59 @@ export const Line = styled.div`
   height: 1px;
   background-color: ${({ theme }) => theme.pureWhiteColor};
   opacity: 0.25;
+`;
+
+export const MobileNavigation = styled.div<MobileNavigationProps>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 254px;
+  height: 100%;
+  background-color: ${({ theme }) => theme.navigationBackgroundColor};
+  backdrop-filter: blur(81px);
+  transform: ${({ isOpen }) => (isOpen ? 'translateX(0)' : 'translateX(200%)')};
+  transition: transform 0.3s;
+  overflow: hidden;
+`;
+
+export const CloseIcon = styled.img.attrs({
+  src: closeIcon,
+})`
+  display: block;
+  margin-top: 34px;
+  margin-left: auto;
+  margin-right: 26px;
+`;
+
+export const MobileLinksContainer = styled.div`
+  padding-top: 65px;
+`;
+
+export const MobileLink = styled(NavLink)`
+  position: relative;
+  display: flex;
+  margin-left: 32px;
+  margin-bottom: 32px;
+  font-family: 'Barlow Condensed', sans-serif;
+  font-size: 16px;
+  line-height: 19px;
+  letter-spacing: 2.7px;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.pureWhiteColor};
+  text-decoration: none;
+
+  &[class*='active']::after {
+    opacity: 1;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 4px;
+    height: 100%;
+    background-color: ${({ theme }) => theme.pureWhiteColor};
+    opacity: 0;
+  }
 `;
